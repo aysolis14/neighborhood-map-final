ffe5423691c1dc08b93a01a4a67defad8b58c0c4
Amanda Solis <aysolis14@gmail.com>
2018-11-05 15:53:53 -0600

    Cleaned up code, removed MainPage.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Map from './Map/Map.js'; 
-import MainPage from './MainPage/MainPage.js';
-import SideMenu from './SideMenu.js';
+import Header from './Header/Header.js';
+import SideMenu from '.SideMenu/SideMenu.js';
+import Map from '.Map/Map.js';
 
+import './MainPage/MainPage.css';
 
 class App extends Component {
+    state = {
+        sideMenuOpen: true,
+        venues: [],
+        markers: [],
+        updateState: element => {
+            this.setState(element)
+        }
+    }
 
-  state = {
-    venues: [],
+    toggleMenu = () => {
+        this.setState(state => ({
+            sideMenuOpen: !state.sideMenuOpen
+        }
+        ))
+    }
 
-  }
-  render() {
-    return (
-      <MainPage />
-    );
-  }
+    markersClose = () => {
+        let markers = this.state.markers.map(marker => {
+            marker.isOpen = false;
+            return marker;
+        })
+        this.setState({ markers: Object.assign(this.state.markers, markers)})
+    }
+
+    markersClick = (marker) => {
+        this.markersClose()
+        marker.isOpen = true;
+        this.setState({
+            markers: Object.assign(this.state.markers, marker)
+        });
+        let venue = this.state.venues.find(venue => venue.id === marker.id)
+        SquareAPI.getVenueDetails(marker.id).then(res => {
+            let fresh = Object.assign(venue, res.response.venue)
+            this.setState({ venues: Object.assign(this.state.venues, fresh)})
+        })
+    }
+
+    sideMenuClick = venue => {
+        let marker = this.state.markers.find(element => element.id === venue.id);
+        this.markersClick(marker)
+    }
 }
 
-export default App;
+export default App; 
